48a10d493d3983cb44c2bbd2d5889a11c1822e99
siste <galaxiaestrellaxl@hotmail.com>
2020-10-14 01:17:32 -0500

    agregar pruebas de test

--- test/javascript/script.js
+++ test/javascript/script.js
@@ -10,16 +10,48 @@ var suma = a + b;
 
 var estado = true;
 
+//objeto
+var person = {
+  firstName: "John",
+  lastName : "Doe",
+  id     : 5566,
+  fullName : function() {
+    return this.firstName + " " + this.lastName;
+  }
+};
+
+//Añadir Evento
+document.getElementById("boton").addEventListener("click", boton);
+
+//Agregar Atributo
+document.getElementById("titulo").setAttribute("class","titulo");
+
+//funcion
+function boton(){
+  alert(person.fullName()); 
+ 
+};
+
 //Obtener contenido
 var texto_titulo = document.getElementById("titulo").innerHTML;
 
 var parrafo = document.getElementsByTagName("p");
 var texto_parrafo = parrafo[0].innerHTML;
 
-//funcion
-function alerta(){
-    alert(texto_parrafo);
-};
+//cambiar contenido
+document.getElementById("titulo").innerHTML = "nuevo titulo";
+
+//cambiar de estilos
+document.getElementById("descripcion1").style.color = "blue";
+
+//fecha
+var fecha = new Date();
+
+
+//Control del Tiempo
+function tiempo(){
+  setTimeout( "alert(screen.width)", 3000)
+}
 
 //condicion
 function condicion(estado){
@@ -66,5 +98,6 @@ switch (new Date().getDay()) {
     case 6:
       day = "Saturday";
   }
+  return day;
 }
 
