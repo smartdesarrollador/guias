c1dfd8087e96d249e0c6412edcd07bc14ef2a542
JEANS <enrique_sistema_5000@hotmail.com>
2020-12-29 18:31:25 -0500

    commit funciones javascript string

---
+++ javascript/funciones/string/longitud.js
@@ -0,0 +1,15 @@
+function longitud(texto) {
+    var sln = texto.length;
+    return sln;
+}
+
+console.log(longitud("mundo"));
+
+
+/*
+function longitud() {
+    var sln = document.getElementById("txt").value;
+    sln = sln.length;
+    document.getElementById("demo").innerHTML = sln;
+}
+*/
